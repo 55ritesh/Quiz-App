fefb0fbb537f74e85a4bab97dc530b4f7e85a16a
Ritesh Kumar <ritesh02072002@gmail.com>
2024-03-26 13:02:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import {useState} from "react";
 import './App.css';
+import Question from './components/Question';
+import question from './constants/question.json';
+import Result from "./components/Result";
 
 function App() {
+
+  const [currentQuestion,setCurrentQuestion] = useState(0);
+  const [userAnswer,setUserAnswer] = useState([]);
+
+  // logic for nnxt question
+
+  const handleNextQuestion=(isCorrect)=>{
+    setCurrentQuestion(currentQuestion+1);
+    setUserAnswer([...userAnswer,isCorrect]);
+  }
+
+  const resetQuiz = () => {
+    setCurrentQuestion(0);
+    setUserAnswer([]);
+  };
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>World Quiz</h1>
+
+     {/* Question component */}
+     {
+      currentQuestion<question.length && (
+      <Question question={question[currentQuestion]}
+       onAnswerClick={handleNextQuestion}
+       />
+      )
+      }
+
+     {/* AnswerOption Component */}
+     {currentQuestion === question.length && (
+        <Result
+          userAnswers={userAnswer}
+          questions={question}
+          resetQuiz={resetQuiz}
+        />
+      )}
+
     </div>
   );
 }
